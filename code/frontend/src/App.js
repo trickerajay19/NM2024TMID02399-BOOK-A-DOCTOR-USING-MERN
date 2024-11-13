ea28f9a03ecfc923a4d617f30bbebd86447d5f40
harishari14 <142790298+harishari14@users.noreply.github.com>
2024-11-13 19:30:26 +0530

    first commit

---
+++ code/frontend/src/App.js
@@ -0,0 +1,42 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {CookiesProvider} from "react-cookie";
+import "./App.css";
+import Home from "./components/common/Home";
+import Login from "./components/common/Login";
+import Register from "./components/common/Register";
+import UserHome from "./components/user/UserHome";
+import AdminHome from "./components/admin/AdminHome";
+import UserAppointments from "./components/user/UserAppointments";
+
+function App() {
+  const userLoggedIn = !!localStorage.getItem("userData");
+  return (
+    <div className="App">
+      <Router>
+        <CookiesProvider>
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/register" element={<Register/>} />
+            {userLoggedIn ? (
+              <>
+                <Route path="/adminhome" element={<AdminHome />} />
+                <Route path="/userhome" element={<UserHome />} />
+                <Route path="/userhome/userappointments/:doctorId" element={<UserAppointments />} />
+              </>
+            ) : (
+              <Route path="/login" element={<Login />} />
+            )}
+          </Routes>
+        </div>
+        <footer className="bg-light text-center text-lg-start">
+          <div className="text-center p-3">© 2023 Copyright: MediCareBook</div>
+          </footer>
+        </CookiesProvider>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
